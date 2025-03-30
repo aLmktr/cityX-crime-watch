0a060fe4ec5cd221a2eec4f3c4cf5814febd40bf
aLmktr <mktr.acc@gmail.com>
2025-03-30 22:33:46 +0400

    🌍 feat: add random crime locations as heamtp for better perforamce

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ import cloudpickle
 import folium
 import pandas as pd
 import streamlit as st
+from folium.plugins import HeatMap
 
 from parse_pdf import extract_police_report_data
 
@@ -21,27 +22,39 @@ with open("crime-classifier.pkl", "rb") as f:
 
 
 # display cityX map
-def create_folium_map(df):
+def create_folium_map(df, sample_size=100000):
+    # compute police distrct apporximate location
     districts = (
         df.groupby("PdDistrict")
         .agg({"latitude": "mean", "longitude": "mean"})
         .reset_index()
     )
 
+    # take random data samples for better perforamnce
+    if len(df) > sample_size:
+        df = df.sample(sample_size, random_state=42)
+
     mean_lat = df["latitude"].mean()
     mean_lon = df["longitude"].mean()
 
-    m = folium.Map(location=[mean_lat, mean_lon], zoom_start=12, control_scale=True)
+    m = folium.Map(
+        location=[mean_lat, mean_lon],
+        zoom_start=12,
+        control_scale=True,
+        tiles="CartoDB dark_matter",
+    )
 
-    # add districts
+    # add disctirct  police departement location
     for _, row in districts.iterrows():
         folium.Marker(
             [row["latitude"], row["longitude"]],
             popup=f"District: {row['PdDistrict']}",
-            icon=folium.Icon(color="blue"),
+            icon=folium.Icon(color="blue", icon="info-sign"),
         ).add_to(m)
 
-    folium.LayerControl().add_to(m)
+    heat_data = df[["latitude", "longitude"]].values.tolist()
+    HeatMap(heat_data, radius=7, blur=5, min_opacity=0.3).add_to(m)
+
     return m
 
 
